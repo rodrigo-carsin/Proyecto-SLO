37e9d4618b34204468d85562c19eb4de95962575
rodrigocarsin <rodrigo.carsin@mercadolibre.com>
2023-05-30 16:38:57 -0300

    some changes

--- Proyecto-SLO/ database slo;.sql
+++ Proyecto-SLO/ database slo;.sql
@@ -1,7 +1,27 @@
-DROP database slo;
-CREATE database slo;
+create database slo;
+
 use slo;
 
+create table proveedores(
+    id int not null primary key auto_increment,
+    cid text not null,
+    nombre_compania text not null,
+    direccion text not null,
+    telefono varchar(15) not null,
+    UNIQUE KEY uk_proveedores_cid (cid(255))
+);
+INSERT INTO `slo`.`proveedores`
+(`id`,
+`cid`,
+`nombre_compania`,
+`direccion`,
+`telefono`)
+VALUES
+(1,
+"company1",
+"company_new",
+"calle1",
+"1111111");
 
 create table depositos(
     id int not null primary key auto_increment,
@@ -9,14 +29,14 @@ create table depositos(
     codigo text not  null,
     capacidad_minima int not null,
     secciones_minimas int not null,
-    UNIQUE KEY uk_depositos_codigo (codigo),
+    UNIQUE KEY uk_depositos_codigo (codigo(255))
 );
 INSERT INTO `slo`.`depositos`
 (`id`,
 `direccion`,
 `codigo`,
-`capacidad_maxima`,
-`secciones_maximas`)
+`capacidad_minima`,
+`secciones_minimas`)
 VALUES
 (1,
 "calle1",
@@ -34,7 +54,7 @@ CREATE TABLE productos (
 	ancho DECIMAL(19,2) NOT NULL,
 	id_tipo_producto INT NOT NULL,
 	id_proveedor INT NOT NULL,
-    UNIQUE KEY uk_productos_codigo (codigo),
+    UNIQUE KEY uk_productos_codigo (codigo(255)),
 	KEY `fk_producto_proveedor` (`id_proveedor`),
 	CONSTRAINT `fk_producto_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -88,34 +108,13 @@ VALUES
 1,
 1);
 
-create table proveedores(
-    id int not null primary key auto_increment,
-    cid text not null,
-    nombre_compania text not null,
-    direccion text not null,
-    telefono varchar(15) not null,
-    UNIQUE KEY uk_proveedores_cid (cid),
-);
-INSERT INTO `slo`.`proveedores`
-(`id`,
-`cid`,
-`nombre_compania`,
-`direccion`,
-`telefono`)
-VALUES
-(1,
-"company1",
-"company_new",
-"calle1",
-"1111111");
-
 create table empleados(
     id int not null primary key auto_increment,
     documento text not null,
     nombre text not null,
     apellido text not null,
     id_deposito int not null,
-    UNIQUE KEY uk_empleados_documento (documento),
+    UNIQUE KEY uk_empleados_documento (documento(255)),
 	KEY `fk_empleados_deposito` (`id_deposito`),
 	CONSTRAINT `fk_empleados_deposito` FOREIGN KEY (`id_deposito`) REFERENCES `depositos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -136,7 +135,8 @@ create table consumidores(
     id int not null primary key auto_increment,
     documento text not null,
     nombre text not null,
-    apellido text not null
+    apellido text not null,
+	UNIQUE KEY uk_consumidores_documento (documento(255))
 );
 INSERT INTO `slo`.`consumidores`
 (`id`,
